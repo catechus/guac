464635ecf0d5a54fc5f4c4f88f81340bc82fb793
Mike Hanes <catechus@gmail.com>
2019-09-28 10:11:14 -0700

    First commit - add recipe.py

---
+++ recipe.py
@@ -0,0 +1,12 @@
+print('''
+
+- 4 avocados
+- 1/4 lemon
+- chopped tomatoes
+- hot sauce
+
+'''
+
+# this is a single line comment
+# starting a command with ''' allows for entering multiple lines of string separated by line breaks
+)
